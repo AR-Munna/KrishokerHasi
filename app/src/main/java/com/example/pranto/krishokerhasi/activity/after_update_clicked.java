27953b6f71d51aef5be7187aa21017a527a4b230
AR-Munna <armunna1997@gmail.com>
2017-11-02 01:31:56 +0600

    second edition

---
+++ app/src/main/java/com/example/pranto/krishokerhasi/activity/after_update_clicked.java
@@ -0,0 +1,56 @@
+package com.example.pranto.krishokerhasi.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.pranto.krishokerhasi.R;
+
+public class after_update_clicked extends AppCompatActivity {
+
+    Button button1,button2,button3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_after_update_clicked);
+
+        TextView textView = (TextView)findViewById(R.id.tv);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    void CreateIntent(String data)
+    {
+        Intent i = new Intent(after_update_clicked.this, InfoPage.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("stuff", data);
+        i.putExtras(bundle);
+        startActivity(i);
+    }
+}
