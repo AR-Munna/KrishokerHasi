ae32a33f93eb8661a87a751abdfbcee0b1def491
AR-Munna <armunna1997@gmail.com>
2017-11-02 23:43:48 +0600

    second edition

---
+++ app/src/main/java/com/example/pranto/krishokerhasi/activity/common_page.java
@@ -0,0 +1,18 @@
+package com.example.pranto.krishokerhasi.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.pranto.krishokerhasi.R;
+
+public class common_page extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_common_page);
+    }
+}
