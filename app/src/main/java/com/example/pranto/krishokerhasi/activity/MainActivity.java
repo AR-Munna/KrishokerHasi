28128028c60250a711409c4ee7060c5da8e83520
AR-Munna <armunna1997@gmail.com>
2017-11-07 20:34:42 +0600

    third edition

--- app/src/main/java/com/example/pranto/krishokerhasi/activity/MainActivity.java
+++ app/src/main/java/com/example/pranto/krishokerhasi/activity/MainActivity.java
@@ -176,30 +176,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_mainpage_drawer, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        /*
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        */
-
-        return super.onOptionsItemSelected(item);
-    }
-
     /*--------------------app theke exit korar jonno "exitFunction" lekha hoise------------------------*/
 
     public void exitFunction()
